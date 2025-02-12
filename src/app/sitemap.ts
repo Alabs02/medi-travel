3ffa1bde2c2c39192cfb1b773763162bdb809139
Alabs02 <alabson.inc@gmail>
2025-02-12 22:42:59 +0100

    updated robots.ts & sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,34 @@
 import { isDevMode } from "@/lib";
+import { useClinics } from "@/hooks";
 
-export const BASE_URL = isDevMode()
-  ? "http://localhost:8050"
-  : "https://meditravel.vercel.app";
+export const BASE_URL =
+  process.env.NEXT_PUBLIC_SITE_URL ||
+  (isDevMode() ? "http://localhost:8050" : "https://meditravel.vercel.app");
+
+export default async function sitemap() {
+  const staticRoutes = ["/", "/auth/login", "/auth/register"].map((route) => ({
+    url: `${BASE_URL}${route}`,
+    lastModified: new Date().toISOString().split("T")[0],
+    changefreq: "weekly",
+    priority: 0.8
+  }));
+
+  const clinicSlugs = await getClinicSlugs();
+
+  const clinicRoutes = clinicSlugs.map((slug) => ({
+    url: `${BASE_URL}/clinic/${slug}`,
+    lastModified: new Date().toISOString().split("T")[0],
+    changefreq: "daily",
+    priority: 0.9
+  }));
+
+  return [...staticRoutes, ...clinicRoutes];
+}
+
+async function getClinicSlugs() {
+  const { data: clinics } = await useClinics().refetch();
+
+  if (!clinics) return [];
+
+  return clinics.map((clinic) => clinic?.clinicSlug);
+}
